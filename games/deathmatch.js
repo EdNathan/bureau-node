c9f5b5dd2e26512c4dda9649c5db711d91a49dad
Ben Tattersley <ubershinysheep@gmail.com>
2014-07-13 19:16:42 +0100

    Implemented deathmatch

--- games/deathmatch.js
+++ games/deathmatch.js
@@ -9,19 +9,20 @@ var deathmatchgame = {
 		return 'Standard pair locking deathmatch'
 	},
 	//Passed all the form data from game setup, should modify the data and return a 'game' object for insertion into the database
-	constructGame: function(data) {
+	constructGame: function(data, callback) {
 		var game = data
 		/* modifications if you're a more complicated game */
-		return game
+		callback(null, game)
 	},
 	
 	//Given a player uid, construct a game state fragment for the player
 	getGameStateForUid: function(game, playerid) {
-		
+		return ''
 	},
 	
 	//Given killer, victim, kill method, time and everything else in the report if needed, determine whether the kill is valid
 	checkKillValid: function(game, killerid, victimid, killmethod, time, report, callback) {
+		//We want to check whether the player has already been killed by this person, or has already killed them
 		this.Bureau.assassin.hasKilledPlayerInGame(killerid, victimid, game.gameid, true, function(err, hasKilled) {
 			if(err) {
 				callback(err)
@@ -30,7 +31,7 @@ var deathmatchgame = {
 					if(err) {
 						callback(err)
 					} else {
-						callback(null, hasKilled || hasBeenKilled)
+						callback(null, !hasKilled && !hasBeenKilled)
 					}
 				})
 			}
@@ -39,22 +40,26 @@ var deathmatchgame = {
 	
 	//Given killer, victim, kill method and the report, handle the kill
 	handleKill: function(game, killerid, victimid, report, callback) {
-		
+		//Add 1 to the score
+		this.Bureau.game.changeScore(game.gameid, killerid, 1, callback)
 	},
 	
 	//Given killer, victim, kill method and the report, undo the effects of the kill (if possible)
 	undoKill: function(game, killerid, victimid, report, callback) {
-		
+		//Remove 1 from the score
+		this.Bureau.game.changeScore(game.gameid, killerid, -1, callback)
 	},
 	
 	//Given the uid of a new player added, handle all in game effects
-	handlePlayerAdded: function(game, playerid) {
-		
+	handlePlayerAdded: function(game, playerid, callback) {
+		//No effect
+		callback(null)
 	},
 	
 	//Given the uid of a player just removed, handle all in game effects
-	handlePlayerRemoved: function(game, playerid) {
-		
+	handlePlayerRemoved: function(game, playerid, callback) {
+		//No effect
+		callback(null)
 	}
 }
 
