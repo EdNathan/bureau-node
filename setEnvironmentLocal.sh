75d1ff4807558944847c011ea6106e6b81c385c0
Ben Tattersley <ubershinysheep@gmail.com>
2015-09-01 17:13:36 +0100

    Failsafe if .env does not exist

--- setEnvironmentLocal.sh
+++ setEnvironmentLocal.sh
@@ -2,8 +2,12 @@
 
 # Set environment variables from .env file
 
-while read ENV_VAR; do
-		VAR_STR="export "
-		VAR_STR+=$ENV_VAR
-		eval $VAR_STR
-done <.env
+if [ -f .env ]; then
+
+	while read ENV_VAR; do
+			VAR_STR="export "
+			VAR_STR+=$ENV_VAR
+			eval $VAR_STR
+	done <.env
+
+fi
