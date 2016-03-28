46bba16b4a6964d7bd8522efb06d12b9c3bbb9f1
Ben Tattersley <ubershinysheep@gmail.com>
2016-03-28 21:18:05 +0100

    login api to get token and USER_ID

--- api/endpoints/auth.js
+++ api/endpoints/auth.js
@@ -1,11 +1,39 @@
 'use strict'
 
+const _ = require( 'lodash' )
+
 module.exports = ( Bureau ) => ( {
 
 	login: ( data, params, callback ) => {
-		console.log( data )
 
-		callback( null, data )
+		let email = _.isString( data.email ) ? data.email : ''
+		let password = _.isString( data.password ) ? data.password : ''
+
+		Bureau.register.loginAssassin( email, password, ( err, assassin ) => {
+
+			if ( err ) {
+				callback( err )
+				return
+			} else if ( !assassin ) {
+				callback( 'Incorrect email or password' )
+				return
+			}
+
+			let uid = assassin._id + ''
+
+			Bureau.assassin.getToken( uid, ( err, token ) => {
+				if ( err ) {
+					callback( err )
+					return
+				}
+
+				callback( null, {
+					USER_ID: uid,
+					USER_TOKEN: token
+				} )
+			} )
+
+		} )
 	}
 
 } )
