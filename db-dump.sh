4374c6526c4307948c357c8673544c9d576e0931
Ben Tattersley <ubershinysheep@gmail.com>
2017-09-27 20:30:14 +0100

    dump scrip

--- db-dump.sh
+++ db-dump.sh
@@ -1,3 +1,3 @@
-mongodump --host $OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT --username $OPENSHIFT_MONGODB_DB_USERNAME --password $OPENSHIFT_MONGODB_DB_PASSWORD
+mongodump
 zip -r db-backup.zip dump
 rm -rf dump
